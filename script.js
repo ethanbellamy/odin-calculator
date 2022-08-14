631851d3ee3d2d784cf817a43700455618b0ecdc
Ethan Bellamy <ethanbellamy@gmail.com>
2022-08-15 00:32:18 +0100

    Split parseInput() into separate functions

--- script.js
+++ script.js
@@ -1,37 +1,3 @@
-function add(a,b) {
-    return a + b;
-}
-
-function subtract(a,b) {
-    return a - b;
-}
-
-function multiply(a,b) {
-    return a * b;
-}
-
-function divide(a,b) {
-    return a / b;
-}
-
-function operate(num1,operator,num2) {
-    if (operator == '+') {
-        return add(+num1,+num2);
-    }
-
-    else if (operator == '-') {
-        return subtract(+num1,+num2);
-    }
-
-    else if (operator == '*') {
-        return multiply(+num1,+num2);
-    }
-
-    else if (operator == '/') {
-        return divide(+num1,+num2);
-    }
-}
-
 let buttons = document.querySelector('#button-container');
 let display = document.querySelector('#display');
 
@@ -45,9 +11,46 @@ let sumCalculated = false;
 
 buttons.addEventListener('click', parseInput);
 
+function operate(num1,operator,num2) {
+    if (operator == '+') {
+        return +num1 + +num2;
+    }
+
+    else if (operator == '-') {
+        return +num1 - +num2;
+    }
+
+    else if (operator == '*') {
+        return +num1 * +num2;
+    }
+
+    else if (operator == '/') {
+        return +num1 / +num2;
+    }
+}
+
 function parseInput(e) {
-    //Number selected
-    if (e.target.classList.contains('number') && displayContent.length < 8) {
+    if (e.target.classList.contains('number')) {
+        numberInput(e);
+    }
+
+    else if (e.target.classList.contains('operator')) {
+        operatorInput(e);
+    }
+
+    //Equals selected
+    else if (e.target.id == 'equal') {
+        equalsInput(e);
+    }
+
+    //Cancel selected
+    else if (e.target.id == 'cancel') {
+        resetCalculation();
+    }
+}
+
+function numberInput(e) {
+    if (displayContent.length < 8) {
         //Clear display after operator selected
         if (operatorSelected == true) {
             display.textContent = displayContent;
@@ -60,63 +63,56 @@ function parseInput(e) {
         }
         
         displayContent += e.target.id;
-        display.textContent = displayContent;
+        display.textContent = displayContent;        
     }
+}
 
-    //Operator selected
-    else if (e.target.classList.contains('operator')) {
-        //Check whether number is entered into display
-        if (!(displayContent == '')) {
-            //If this is first number entered
-            if (firstOperand == null) {
-                firstOperand = displayContent;
-                operator = e.target.id;
-            }
-
-            //If sum is already calculated
-            else if (!(sum == null)) {
-                firstOperand = sum;
-                operator = e.target.id;
-                sum = null;
-                secondOperand = null;
-            }
-
-            //If first number is already entered, but sum is not calculated yet,
-            //then calculate previous sum and assign to firstOperand for use in next sum
-            else {
-                secondOperand = displayContent;
-                firstOperand = operate(firstOperand, operator, secondOperand);
-                display.textContent = firstOperand;
-                operator = e.target.id;
-                secondOperand = null;
-            }
-
-            operatorSelected = true;
-            displayContent = '';
+function operatorInput(e) {
+    //Check whether number is entered into display
+    if (!(displayContent == '')) {
+        //If this is first number entered
+        if (firstOperand == null) {
+            firstOperand = displayContent;
+            operator = e.target.id;
         }
-    }
 
-    //Equals selected
-    else if (e.target.id == 'equal') {
-        //Check there are two operands before calculating sum
-        if (!(firstOperand == null) && !(displayContent == null)) {
-            secondOperand = displayContent;
-            sum = operate(firstOperand, operator, secondOperand);
-
-            firstOperand = null;
+        //If sum is already calculated
+        else if (!(sum == null)) {
+            firstOperand = sum;
+            operator = e.target.id;
+            sum = null;
             secondOperand = null;
-
-            displayContent = sum;
-            display.textContent = displayContent;
-
-            displayContent = '';
-            sumCalculated = true;
         }
-    }
 
-    //Cancel selected
-    else if (e.target.id == 'cancel') {
-        resetCalculation();
+        //If first number is already entered, but sum is not calculated yet,
+        //then calculate previous sum and assign to firstOperand for use in next sum
+        else {
+            secondOperand = displayContent;
+            firstOperand = operate(firstOperand, operator, secondOperand);
+            display.textContent = firstOperand;
+            operator = e.target.id;
+            secondOperand = null;
+        }
+
+        operatorSelected = true;
+        displayContent = '';
+    }        
+}
+
+function equalsInput(e) {
+    //Check there are two operands before calculating sum
+    if (!(firstOperand == null) && !(displayContent == null)) {
+        secondOperand = displayContent;
+        sum = operate(firstOperand, operator, secondOperand);
+
+        firstOperand = null;
+        secondOperand = null;
+
+        displayContent = sum;
+        display.textContent = displayContent;
+
+        displayContent = '';
+        sumCalculated = true;
     }
 }
 
