2d109bce61c40062ab125ed220407c3263591095
Ethan Bellamy <ethanbellamy@gmail.com>
2022-08-14 21:39:36 +0100

    Perform respective actions when operator, equal and cancel buttons are pressed

--- script.js
+++ script.js
@@ -16,29 +16,94 @@ function divide(a,b) {
 
 function operate(num1,operator,num2) {
     if (operator == '+') {
-        return add(num1,num2);
+        return add(+num1,+num2);
     }
 
     else if (operator == '-') {
-        return subtract(num1,num2);
+        return subtract(+num1,+num2);
     }
 
     else if (operator == '*') {
-        return multiply(num1,num2);
+        return multiply(+num1,+num2);
     }
 
     else if (operator == '/') {
-        return divide(num1,num2);
+        return divide(+num1,+num2);
     }
 }
 
 let buttons = document.querySelector('#button-container');
 let display = document.querySelector('#display');
-let displayContent = '';
 
-buttons.addEventListener('click', e => {
+let displayContent = '';
+let firstOperand = null;
+let operator = null;
+let secondOperand = null;
+let sum = null;
+
+buttons.addEventListener('click', parseInput);
+
+function parseInput(e) {
+    //Number selected
     if (e.target.classList.contains('number') && displayContent.length < 9) {
         displayContent += e.target.id;
         display.textContent = displayContent;
     }
-})
+
+    //Operator selected
+    else if (e.target.classList.contains('operator')) {
+        //Check whether number is entered into display
+        if (!(displayContent == null)) {
+            operator = e.target.id;
+
+            //If this is first number entered
+            if (firstOperand == null) {
+                firstOperand = displayContent;
+            }
+
+            //If sum is already calculated
+            else if (!(sum == null)) {
+                firstOperand = sum;
+                sum = null;
+                secondOperand = null;
+            }
+
+            //If first number is already entered, but sum is not calculated yet
+            else {
+                secondOperand = displayContent;
+                firstOperand = operate(firstOperand, operator, secondOperand);
+                secondOperand = null;
+            }
+
+            //Clear display
+            displayContent = '';
+            display.textContent = displayContent;
+        }
+    }
+
+    //Equals selected
+    else if (e.target.id == '=') {
+        //Check there are two operands before calculating sum
+        if (!(firstOperand == null) && !(displayContent == null)) {
+            secondOperand = displayContent;
+            sum = operate(firstOperand, operator, secondOperand);
+
+            firstOperand = null;
+            secondOperand = null;
+
+            displayContent = sum;
+            display.textContent = displayContent;
+        }
+    }
+
+    //Cancel selected
+    else if (e.target.id == 'C') {
+        displayContent = '';
+        display.textContent = displayContent;
+
+        firstOperand = null;
+        operator = null;
+        secondOperand = null;
+        sum = null;
+    }
+}
